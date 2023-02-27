f4628eac29113ac1c798a6163eda6ba99a7a8f65
Vaayne <liu.vaayne@gmail.com>
2023-02-27 22:24:07 +0800

    Adjust the color of the button.

--- notionai-plus/src/content.tsx
+++ notionai-plus/src/content.tsx
@@ -146,7 +146,7 @@ const Index = () => {
         <div className="fixed top-1/3 right-10 w-1/3 h-1/2 overflow-hidden rounded-lg flex flex-col bg-blue-100">
           <div className="form-control">
             <label className="label">
-              <span className="label-text">
+              <span className="label-text text-primary-focus">
                 Here is your context that you want NotionAI to process
               </span>
             </label>
@@ -157,7 +157,7 @@ const Index = () => {
               onChange={(e) => setContext(e.target.value)}></textarea>
           </div>
           <label className="label">
-            <span className="label-text">
+            <span className="label-text text-primary-focus">
               Here is your Prompt that how you want NotionAI process the context{" "}
             </span>
           </label>
@@ -172,36 +172,36 @@ const Index = () => {
             Submit
           </button>
           <div className="divider m-0"></div>
-          <div className="flex flex-col h-full">
-            <div className="p-0 m-0 flex flex-row justify-between content-center items-stretch">
-              <h3 className="px-4 my-1 self-center">NotionAI Says: </h3>
-              <div className="flex flex-row">
-                <button className="btn gap-2" onClick={handleClear}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    width="16"
-                    height="16">
-                    <path d="M17.7,6.3c-0.6-0.6-1.6-0.6-2.2,0c-2.2,2.2-5.7,2.2-7.9,0c-0.6-0.6-1.6-0.6-2.2,0c-0.6,0.6-0.6,1.6,0,2.2c3.4,3.4,8.9,3.4,12.3,0C18.3,7.9,18.3,6.9,17.7,6.3z M13.8,11.7c-0.6,0.6-0.6,1.6,0,2.2c0.6,0.6,1.6,0.6,2.2,0c1.5-1.5,1.5-4,0-5.5c-0.6-0.6-1.6-0.6-2.2,0C13.2,10.1,13.2,11.1,13.8,11.7z M9.3,13.3c0.6-0.6,0.6-1.6,0-2.2c-0.6-0.6-1.6-0.6-2.2,0c-1.5,1.5-1.5,4,0,5.5c0.6,0.6,1.6,0.6,2.2,0C9.9,14.9,9.9,13.9,9.3,13.3z" />
-                  </svg>
-                </button>
-                <button className="btn gap-2" onClick={handleCopy}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    width="16"
-                    height="16">
-                    <path d="M19.4,4.6h-4.1V3c0-1.1-0.9-2-2-2s-2,0.9-2,2v1.6h-4.1c-1.1,0-2,0.9-2,2v13.8c0,1.1,0.9,2,2,2h14.1c1.1,0,2-0.9,2-2V6.6C21.4,5.5,20.5,4.6,19.4,4.6z M7.4,3c0-0.6,0.4-1,1-1s1,0.4,1,1v1.6H7.4V3z M15.3,18.4H8.7v-1.6c0-0.6-0.4-1-1-1s-1,0.4-1,1v1.6H4.7V6.6h10.6V18.4z" />
-                  </svg>
-                </button>
-              </div>
+          <div className="p-0 m-0 flex flex-row justify-between content-center items-stretch">
+            <h3 className="px-4 my-1 self-center text-primary">
+              NotionAI Says:
+            </h3>
+            <div className="flex flex-row">
+              <button className="btn btn-primary gap-2" onClick={handleClear}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  width="16"
+                  height="16">
+                  <path d="M17.7,6.3c-0.6-0.6-1.6-0.6-2.2,0c-2.2,2.2-5.7,2.2-7.9,0c-0.6-0.6-1.6-0.6-2.2,0c-0.6,0.6-0.6,1.6,0,2.2c3.4,3.4,8.9,3.4,12.3,0C18.3,7.9,18.3,6.9,17.7,6.3z M13.8,11.7c-0.6,0.6-0.6,1.6,0,2.2c0.6,0.6,1.6,0.6,2.2,0c1.5-1.5,1.5-4,0-5.5c-0.6-0.6-1.6-0.6-2.2,0C13.2,10.1,13.2,11.1,13.8,11.7z M9.3,13.3c0.6-0.6,0.6-1.6,0-2.2c-0.6-0.6-1.6-0.6-2.2,0c-1.5,1.5-1.5,4,0,5.5c0.6,0.6,1.6,0.6,2.2,0C9.9,14.9,9.9,13.9,9.3,13.3z" />
+                </svg>
+              </button>
+              <button className="btn btn-primary gap-2" onClick={handleCopy}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  width="16"
+                  height="16">
+                  <path d="M19.4,4.6h-4.1V3c0-1.1-0.9-2-2-2s-2,0.9-2,2v1.6h-4.1c-1.1,0-2,0.9-2,2v13.8c0,1.1,0.9,2,2,2h14.1c1.1,0,2-0.9,2-2V6.6C21.4,5.5,20.5,4.6,19.4,4.6z M7.4,3c0-0.6,0.4-1,1-1s1,0.4,1,1v1.6H7.4V3z M15.3,18.4H8.7v-1.6c0-0.6-0.4-1-1-1s-1,0.4-1,1v1.6H4.7V6.6h10.6V18.4z" />
+                </svg>
+              </button>
             </div>
-            <div className="divider m-0"></div>
-            <div className="flex-1 px-4 pb-4 w-full overflow-auto box-border ">
-              {handleLoading()}
-            </div>
+          </div>
+          <div className="divider m-0"></div>
+          <div className="px-4 overflow-auto box-border ">
+            {handleLoading()}
           </div>
         </div>
       )
