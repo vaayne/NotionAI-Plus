f63efddd79846a225b00af75d05849c9244a3f29
Vaayne <liu.vaayne@gmail.com>
2023-03-06 14:11:09 +0800

    add full mode

--- notionai-plus/src/content.tsx
+++ notionai-plus/src/content.tsx
@@ -1,7 +1,14 @@
 import "~base.css"
 
 import cssText from "data-text:~style.css"
-import { ClipboardCopy, Eraser, Github, Twitter } from "lucide-react"
+import {
+  ClipboardCopy,
+  Eraser,
+  Expand,
+  Github,
+  Shrink,
+  Twitter
+} from "lucide-react"
 import { marked } from "marked"
 import type { PlasmoCSConfig } from "plasmo"
 import { useEffect, useState } from "react"
@@ -41,6 +48,7 @@ const Index = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [isShowElement, setIsShowElement] = useState(false)
   const [notification, setNotification] = useState<string>("")
+  const [isFullMode, setIsFullMode] = useState<boolean>(false)
 
   // hidden panel using ESC
   useEffect(() => {
@@ -82,12 +90,15 @@ const Index = () => {
 
   const handleLoading = () => {
     if (isLoading) {
-      return <progress className="progress w-56"></progress>
+      return (
+        <progress
+          className={`progress ${isFullMode ? "w-full" : "w-56"}`}></progress>
+      )
     }
     const html = marked(responseMessage)
     return (
       <article
-        className="prose-xs max-w-none"
+        className={`${isFullMode ? "prose-base" : "prose-xs"} `}
         dangerouslySetInnerHTML={{ __html: html }}></article>
     )
   }
@@ -152,10 +163,32 @@ const Index = () => {
     handleToast("Cleared")
   }
 
+  const renderFullMode = () => {
+    const mode = () => {
+      if (isFullMode) {
+        return <Shrink />
+      } else {
+        return <Expand />
+      }
+    }
+
+    return (
+      <button
+        className="btn bg-transparent border-0"
+        onClick={() => setIsFullMode(!isFullMode)}>
+        {mode()}
+      </button>
+    )
+  }
+
   const page = () => {
     if (isShowElement) {
       return (
-        <div className="fixed top-1/3 right-10 h-1/2 w-1/3 min-w-64 overflow-hidden rounded-lg flex flex-col bg-base-300 dark:bg-slate-500">
+        <div
+          id="notionai-plus"
+          className={`fixed right-10 ${
+            isFullMode ? "h-5/6 w-11/12 top-10" : "h-1/2 w-1/3 top-1/3 "
+          } min-w-64 overflow-hidden rounded-lg flex flex-col bg-base-300 dark:bg-slate-500`}>
           <div className="form-control flex flex-col">
             <div className="flex flex-row justify-between">
               <label className="label">
@@ -168,24 +201,27 @@ const Index = () => {
                   className="px-2">
                   <Twitter />
                 </a>
-
                 <a
                   href="https://github.com/Vaayne/NotionAI"
                   target="_blank"
                   className="bg-accent-transparent">
                   <Github />
                 </a>
+                {renderFullMode()}
               </div>
             </div>
 
             <textarea
-              className="textarea mx-1 break-all text-xs rounded-lg dark:bg-info-content dark:text-white"
+              className={`textarea mx-1 break-all ${
+                isFullMode ? "text-sm" : "text-xs"
+              }  rounded-lg dark:bg-info-content dark:text-white`}
               placeholder="Please enter your context"
               value={context}
               onChange={(e) => setContext(e.target.value)}></textarea>
           </div>
           <div className="flex flex-row items-center">
             <SelectComponent
+              isFullMode={isFullMode}
               selectedPrompt={selectedPrompt}
               setSelectedPrompt={setSelectedPrompt}
               prompt={prompt}
@@ -193,24 +229,33 @@ const Index = () => {
             />
 
             <button
-              className="btn-xs btn-base-300 ml-0 m-2 rounded-lg dark:bg-info-content dark:text-white"
+              className={`${
+                isFullMode ? "btn-sm" : "btn-xs"
+              } btn-base-300 ml-0 m-2 rounded-lg dark:bg-info-content dark:text-white`}
               onClick={handleMessage}>
               Submit
             </button>
           </div>
           <div className="divider m-0"></div>
           <div className="p-0 m-0 flex flex-row justify-between content-center items-stretch">
-            <p className="px-4 my-1 self-center text-sm text-black dark:text-white">
+            <p
+              className={`px-4 my-1 self-center ${
+                isFullMode ? "text-sm" : "text-xs"
+              } text-black dark:text-white`}>
               <strong>AI Says:</strong>
             </p>
             <div className="flex flex-row">
               <button
-                className="btn-xs btn-primary bg-transparent border-0 gap-2"
+                className={`${
+                  isFullMode ? "btn-sm" : "btn-xs"
+                } btn-primary bg-transparent border-0 gap-2`}
                 onClick={handleClear}>
                 <Eraser />
               </button>
               <button
-                className="btn-xs btn-primary bg-transparent border-0 gap-2"
+                className={`${
+                  isFullMode ? "btn-sm" : "btn-xs"
+                } btn-primary bg-transparent border-0 gap-2`}
                 onClick={handleCopy}>
                 <ClipboardCopy />
               </button>
